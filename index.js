cb20b12b95815bfa1dd02107c4f6dce7c0ffc28e
LoopFruits <Kensworldc@gmail.com>
2022-02-04 11:21:46 -0500

    commit tree 38a170f38e61853f46df0c67ecf4888ff2ed226e

--- index.js
+++ index.js
@@ -5,8 +5,31 @@ const employee = {
     streetAddress: '123 Broadway',
 }
 
+
+// we're using spread operator here. Copying all the elements of an existing arry into a new array.
+// Creating a clone of the original object and save it into a new variable. 
 function updateEmployeeWithKeyAndValue(employee, key, value){
     const newObj = {...employee}
     newObj[key] = value;
     return newObj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObject = Object.assign({}, employee)
+    console.log('line25:', employee, key);
+    delete newObject[key];
+    console.log('line27:', employee, key);
+    return newObject;
+    console.log('line:', employee, key)
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee;
 }
