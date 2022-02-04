824239bc432342440e32495a0bff9b918e180c88
LoopFruits <Kensworldc@gmail.com>
2022-02-04 11:00:22 -0500

    passed first test

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
 // Write your solution in this file!
+
 const employee = {
-    name: "Tim",
-    streetAddress: "9109 222nd ST",
-};
+    name: 'Tim',
+    streetAddress: '123 Broadway',
+}
 
-
-function updateEmployeeWithKeyAndValue(employee, key, value) {
-    const newObj = {...employee};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = {...employee}
     newObj[key] = value;
-    return  newObj;
-}
-
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    return employee;
-}
-
-function deleteFromEmployeeByKey(employee, key) {
-    const newObject = Object.assign({}, employee)
-    delete newObject[key];
-    return newObject;
-}
-
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key]
-    return employee;
+    return newObj
 }
